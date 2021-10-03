b9c3b1db3c68c889191228c2705df4d9a1bdedc8
tsha481 <81075217+tsha481@users.noreply.github.com>
2021-10-03 19:06:48 +1300

    made some statements comments and fixed indexing in loop

--- routegeneration.py
+++ routegeneration.py
@@ -14,7 +14,7 @@ from itertools import combinations
 
 arr = list(range(10))
 
-#print(list(combinations(arr, 4)))
+#print(list(combinations(arr, 2)))
 
 data = pd.read_csv('WoolworthsStores.csv')
 data = data[1:66]
@@ -25,24 +25,24 @@ reg1, reg2, reg3, reg4, reg5, reg6 = {}, {}, {}, {}, {}, {}
 
 region_counter = 1
 
-print(data.iloc[0]['Region'])
-print(data.iloc[0]['Store'])
+#print(data.iloc[0]['Region'])
+#print(data.iloc[0]['Store'])
 
 for i in range(len(data)-1):
     store_name = str(data.iloc[i]['Store'])
     region_number = int(data.iloc[i]['Region'])
     if region_number == 1:
-        reg1[store_name] = i
+        reg1[store_name] = i+1
     elif region_number == 2:
-        reg2[store_name] = i
+        reg2[store_name] = i+1
     elif region_number == 3:
-        reg3[store_name] = i
+        reg3[store_name] = i+1
     elif region_number == 4:
-        reg4[store_name] = i
+        reg4[store_name] = i+1
     elif region_number == 5:
-        reg5[store_name] = i
+        reg5[store_name] = i+1
     else:
-        reg6[store_name] = i
+        reg6[store_name] = i+1
     
 print(reg1)
 
